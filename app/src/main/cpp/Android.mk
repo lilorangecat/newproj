6951c36d893019e0c27d989c04a892bbd47660f2
Your Name <you@example.com>
2025-05-07 16:38:55 +0000

    changes6

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,4 +1,37 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := WIDGETS_EXPERT
+
+LOCAL_CFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -fpermissive -fexceptions
+LOCAL_CPPFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -Werror -std=c++17
+LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fpermissive -Wall -fexceptions
+LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all,-llog
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
+LOCAL_ARM_MODE := arm
+
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui/backends
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+
+LOCAL_SRC_FILES := main.cpp \
+	ImGui/imgui.cpp \
+    ImGui/imgui_draw.cpp \
+    ImGui/imgui_widgets.cpp \
+    ImGui/imgui_tables.cpp \
+    ImGui/backends/imgui_impl_opengl3.cpp \
+    ImGui/backends/imgui_impl_android.cpp \
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mc
--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,1 +1,7 @@
-APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
+APP_ABI := armeabi-v7a arm64-v8a
+# APP_PLATFORM := android-18 
+APP_STL := c++_static
+APP_OPTIM := release
+APP_THIN_ARCHIVE := true
+APP_PIE         := true
+
