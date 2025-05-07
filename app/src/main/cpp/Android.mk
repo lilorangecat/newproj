f35ad07ecfe4dee64c76f407e136345cd80918dd
Your Name <you@example.com>
2025-05-07 16:43:39 +0000

    changes7

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,3 +1,11 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mc
+LOCAL_SRC_FILES := mc-init.cpp
+include $(BUILD_SHARED_LIBRARY)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -31,9 +39,3 @@ LOCAL_SRC_FILES := main.cpp \
 include $(BUILD_SHARED_LIBRARY)
 
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mc
-LOCAL_SRC_FILES := mc-init.cpp
-include $(BUILD_SHARED_LIBRARY)
